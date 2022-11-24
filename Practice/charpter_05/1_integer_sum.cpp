52ba3a6ba275500db18eb74ccbcbdb8d9c7f89f3
HansenBurger <1035376945@qq.com>
2022-11-24 20:48:54 +0800

    charpter 5 review

---
+++ Practice/charpter_05/1_integer_sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    int i_start, i_end;
+
+    cout << "Enter two integers and calculate the sum between them: " << endl;
+    cout << "Integer 1: ";
+    cin >> i_start;
+    cout << "Integer 2: ";
+    cin >> i_end;
+    return 0;
+}
---
+++ Practice/charpter_05/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    char ch;
+    ch = cin.get();
+    cout << ch << endl;
+}
